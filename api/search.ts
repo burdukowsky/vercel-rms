bafce3c0dbf6d5ca79b0869b28fa2bd7ddd6a6df
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2024-03-02 17:20:40 +0700

    validate query

--- api/search.ts
+++ api/search.ts
@@ -1,11 +1,21 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse): VercelResponse {
+  const empty = () => res.json({});
+
   const { q } = req.query;
-  if (q.length === 0) {
-    return res.json({});
+
+  if (q == null || Array.isArray(q)) {
+    return empty();
   }
+
+  const query = q.trim();
+
+  if (query.length === 0) {
+    return empty();
+  }
+
   return res.json({
-    message: `Hello ${q}!`,
+    message: `Hello ${query}!`,
   });
 }
